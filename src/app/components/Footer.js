b5d48eb7181544f34fe6d7c276f9248abf8fc6ee
Hrushi4151 <108774635+Hrushi4151@users.noreply.github.com>
2024-01-29 09:55:14 +0530

    Update Footer.js

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -11,7 +11,7 @@ const Footer = () => {
     <div>
     <footer className="bg-white">
         <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
-            <div className="flex justify-between items-center">
+            <div className="flex flex-wrap justify-between items-center">
               <div className="mb-6 md:mb-0 flex flex-row justify-center items-center">
 			<Image src={image} width={50} height={50} alt=""/>
 			<p className='text-2xl text-center text-white font-bold ml-3 bg-red-500 w-auto h-auto py-1 pr-2'><span className=' bg-black text-white px-2 py-1'>Bluechip</span> Art </p>
