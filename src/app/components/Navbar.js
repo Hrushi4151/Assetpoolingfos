7489b725fc30bf442bad6dc642ff7f29e5b58a46
Hrushi4151 <108774635+Hrushi4151@users.noreply.github.com>
2024-01-29 10:25:47 +0530

    Update Navbar.js

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -93,7 +93,7 @@ const Navbar = () => {
                 Art{" "}
               </p>
             </div>
-            <div className="lg:hidden">
+            <div className="lg:hidden text-right">
               <button
                 onClick={() => setToggle(true)}
                 className="navbar-burger flex items-center text-blue-600 p-3"
